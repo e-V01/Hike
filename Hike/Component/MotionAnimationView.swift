daf3f25e3bc8443066bef0431a261fa01b91259e
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-15 15:48:56 +0200

    custom animation for ZStack inner circle

---
+++ Hike/Component/MotionAnimationView.swift
@@ -0,0 +1,58 @@
+//
+//  MotionAnimationView.swift
+//  Hike
+//
+//  Created by Y K on 15.12.2023.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    // MARK: - properties
+    @State private var randomCircle: Int = Int.random(in: 6...12)
+    @State private var isAnimating: Bool = false
+    // MARK: - func
+    // 1. random coord
+    func randomCoordinate() -> CGFloat {
+        return CGFloat.random(in: 0...256)
+    }
+    // 2. random size
+    func randomSize() -> CGFloat {
+        return CGFloat(Int.random(in: 4...80))
+    }
+    // 3. random scale
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    // 4. random speed
+    // 5. random delay
+    var body: some View {
+        ZStack {
+            ForEach(0...randomCircle, id: \.self) { item in
+                Circle()
+                    .foregroundStyle(.white)
+                    .opacity(0.25)
+                    .frame(width: randomSize())
+                    .position(x: randomCoordinate(),
+                              y: randomCoordinate()
+                    )
+                    .scaleEffect(isAnimating ? randomScale() : 1)
+                    .onAppear(perform: {
+                        withAnimation(.interpolatingSpring(stiffness: 0.25, damping: 0.25)
+                            .repeatForever()
+                        ) {
+                            isAnimating = true
+                        }
+                    })
+            }
+        }
+        .frame(width: 256, height: 256)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.teal.ignoresSafeArea()
+        MotionAnimationView()
+    }
+}
