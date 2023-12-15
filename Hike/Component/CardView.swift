adff7a990a4d263da6685a2b2d9ae302b216123d
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-15 14:30:17 +0200

    commit

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,23 +8,87 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - Properties
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+
+    // MARK: - Fucntions
+    func randomImage() {
+        print("The button was pressed")
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber 
+            imageNumber = randomNumber
+    }
     var body: some View {
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(.linearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")
-                                                  ],
-                                          startPoint: .topLeading,
-                                          endPoint: .bottomTrailing
-                                         )
-                    )
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
-            }
-        }
+            VStack {
+                // MARK: - Header
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.customGrayLight,
+                                             . customGrayMedium], 
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                        )
+                        Spacer()
+                        Button {
+                            print("Button was pressed")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundStyle(Color.customGrayMedium)
+                } //: - Header
+                .padding(.horizontal, 30)
+                
+                // MARK: - Main Content
+                ZStack {
+                    Circle()
+                        .fill(.linearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")
+                                                      ],
+                                              startPoint: .topLeading,
+                                              endPoint: .bottomTrailing
+                                             )
+                        )
+                        .frame(width: 256, height: 256)
+                    Image("image-\(imageNumber)")
+                        .resizable()
+                        .scaledToFit()
+                }
+                // MARK: - Footer
+                Button {
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(colors: [.customGreenLight,
+                                .customGreenMedium],
+                                           startPoint: .top,
+                                           endPoint: .bottom
+                            )
+                        )
+                        .shadow(color: .black.opacity(0.3), radius: 0.25, x: 1, y: 2)
+                    
+                }
+                .buttonStyle(GradientButtonStyle())
+                
+                
+                
+            } // MARK: VStack
+            
+        } // MARK: - Card
         .frame(width: 320, height: 570)
     }
 }
--- Hike/Utility/GradientButtonStyle.swift
+++ Hike/Utility/GradientButtonStyle.swift
@@ -4,5 +4,35 @@
 //
 //  Created by Y K on 15.12.2023.
 //
+// COnditional Statement
+// Conditional statement ? A: B
+// A: when user pressed the button
+// B: when button is not pressed
 
+import SwiftUI
 import Foundation
+
+struct GradientButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                configuration.isPressed ? LinearGradient(colors:
+                                                            [.customGrayMedium,
+                                                                .customGrayLight],
+                                                           startPoint: .top,
+                                                           endPoint: .bottom)
+                :
+                LinearGradient(colors:
+                                        [.customGrayLight,
+                                            .customGrayMedium],
+                                       startPoint: .top,
+                                       endPoint: .bottom)
+            )
+            .cornerRadius(40)
+    }
+}
+
+
