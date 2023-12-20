8cec575bf3f45e46e3c6c35912643b8d279ac6e7
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-20 14:02:57 +0200

    To make an adjustable icon you have to get to your prohect menu, then check launch icon, then inside build settings find asset icon catalog and manually add one by one every signle icon thatconforms to the same name as it is in your catalogue

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -81,6 +81,13 @@ struct SettingsView: View {
                         ForEach(alternateAppIcons.indices, id: \.self) { item in
                             Button {
                                 print("Pressed icon \(alternateAppIcons[item])")
+                                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) { error in
+                                 if error != nil {
+                                     print("DEBUG: Failed request to update the app's icon: \( String(describing: error?.localizedDescription))")
+                                } else {
+                                    print("Success! You have changed the app's icon to \(alternateAppIcons[item])")
+                                }
+                                }
                             } label: {
                                 Image("\(alternateAppIcons[item])-Preview")
                                     .resizable()
